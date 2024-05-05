f73cd713edba16e186953c3614e2c6eccedee3e9
Didier Drobecq <didier@drobecq.fr>
2024-05-05 22:46:45 +0200

    init
    useLocalStorage hook

---
+++ index.js
@@ -0,0 +1,48 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+
+function getStorage(storageKey, defaultValue) {
+    try {
+        if (typeof window !== 'undefined') {
+            const storedValue = window.localStorage.getItem(storageKey);
+            if (storedValue === null) {
+                return defaultValue;
+            } else {
+                return JSON.parse(storedValue);
+            }
+        }
+    } catch (error) {
+        console.error('getStorage', 'get', error);
+        return defaultValue;
+    }
+    return defaultValue;
+}
+
+// **********************************************
+// function : useLocalStorage
+//  storageKey : key to get back the stored content
+//  defaultValue : default content to get if no content is stored
+// description : React hook to set and get content in local storage
+// **********************************************
+function useLocalStorage (storageKey, defaultValue) {
+  const [value, setValue] = useState (initValue());
+  
+  function initValue() {
+    return getStorage(storageKey, defaultValue);
+  }
+
+  useEffect(() => {
+    try {
+      if (typeof window !== 'undefined') {
+        window.localStorage.setItem(storageKey, JSON.stringify(value));
+      }
+    } catch (error) {
+      console.error('useLocalStorage', 'set', error);
+    }
+  }, [value, storageKey]);
+  
+  return [value, setValue];
+}
+
+module.exports = useLocalStorage
